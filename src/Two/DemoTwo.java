077dc4bf0d6e23ba837195a3e8ded0d2289aff50
mink0 <mink0@DESKTOP-49NJAGQ.fios-router.home>
2021-11-23 00:26:12 -0500

    Reverse String Code

--- src/Two/DemoTwo.java
+++ src/Two/DemoTwo.java
@@ -4,8 +4,24 @@ public class DemoTwo {
 
 	public static void main(String[] args) {
 
-System.out.println("Code Two Commit");
+		String str = "Hello Github";
 
+		char[] strArray = str.toCharArray();
+
+		for (int i = 0; i < strArray.length; i++) {
+
+			System.out.println(strArray[i]);
+
+		}
+		System.out.println("-----------------");
+		for (int n = strArray.length-1; n >= 0; n--) {
+            
+			System.out.println (strArray[n]);
+             
+			
+
+             
+		
 	}
 
-}
+}}
